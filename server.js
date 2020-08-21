3a732df820ec3287a7d64c1e38f544846d9e6569
Cláudio Medeiros <klawdyo@gmail.com>
2020-08-21 16:47:18 -0300

    alteração no nome

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http')
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     console.log('nova conexao')
-    res.end('hello awesome - now with continuous deployment')
+    res.end('Continuous Integration concluída com sucesso')
 })
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, ()=>console.log('listening'))
+server.listen(PORT, () => console.log('listening'))
