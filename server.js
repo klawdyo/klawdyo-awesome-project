64dd1c713d8288c002412222f178f0771e59aefa
Claudio Medeiros <klawdyo@users.noreply.github.com>
2020-08-21 16:36:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) =>{
     console.log('nova conexao')
-    res.end('hello awesome2')
+    res.end('hello awesome - now with continuous deployment')
 })
 
 const PORT = process.env.PORT || 8080;
