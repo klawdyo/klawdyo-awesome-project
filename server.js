f94f5fcb082e33af0487c7803a9c160a5908532e
Cláudio Medeiros <klawdyo@gmail.com>
2020-08-21 17:22:06 -0300

    alteração da versão de produção

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('nova conexao')
-    res.end('Continuous Integration concluída com sucesso - Staging version')
+    res.end('Continuous Integration concluída com sucesso - Production version')
 })
 
 const PORT = process.env.PORT || 8080;
