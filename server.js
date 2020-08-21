7e6f76cc5909c68dd6b45e35a9039d2bb9010111
Cláudio Medeiros <klawdyo@gmail.com>
2020-08-21 17:27:40 -0300

    asd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('nova conexao')
-    res.end('Continuous Integration concluída com sucesso - Production version')
+    res.end('Continuous Integration concluída com sucesso - Production version 2')
 })
 
 const PORT = process.env.PORT || 8080;
